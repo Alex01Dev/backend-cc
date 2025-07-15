63b4b2104ad616a3214ee8bf6473cb163b3c6cdc
Alex01Dev <alex.mauri.mc@gmail.com>
2025-07-14 22:01:30 -0600

    delete: borrando los archivos de purchase porque la logica se cambia a interaction

--- schemas/purchaseSchemas.py
+++
@@ -1,7 +0,0 @@
-from marshmallow import Schema, fields
-
-class PurchaseSchema(Schema):
-    id = fields.Int(dump_only=True)
-    user_id = fields.Int(required=True)
-    product_id = fields.Int(required=True)
-    date = fields.DateTime(dump_only=True)
