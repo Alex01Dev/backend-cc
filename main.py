d7430e602c11fd87919491da5d063a8a8eabe43f
Alex01Dev <alex.mauri.mc@gmail.com>
2025-07-14 22:02:56 -0600

    delete: borrando el routes de recomendaciones y quitando del main

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from routes.productsRoutes import product_router
 from routes.usersRoutes import user
 from fastapi.middleware.cors import CORSMiddleware
 from config.db import Base, engine
-from routes.recommendationRoutes import recomendacion
 from routes.interactionRoutes import interaction
 from routes.training import training_router
 
@@ -19,6 +18,5 @@ app = FastAPI(
 # Registrar rutas
 app.include_router(product_router)
 app.include_router(user)
-app.include_router(recomendacion)
 app.include_router(interaction)
 app.include_router(training_router)
